c67638315b9766fcba50b1e2636dc3b18257f298
Feiyue Chen <Feiyue.Chen@verisilicon.com>
2024-02-19 08:03:37 +0000

    Added pool/matmul/flatten ops

--- onnxruntime/core/providers/vsinpu/builders/impl/pool_op_builder.h
+++ onnxruntime/core/providers/vsinpu/builders/impl/pool_op_builder.h
@@ -22,26 +22,119 @@
  *
  *****************************************************************************/
 #include "core/providers/vsinpu/builders/impl/base_op_builder.h"
+#include "core/providers/shared/utils/utils.h"
 
 namespace onnxruntime {
 namespace vsi {
 namespace npu {
-class GlobalAveragePoolOpBuilder : public BaseOpBuilder {
+class BasePoolOpBuilder : public BaseOpBuilder {
+ public:
+  BasePoolOpBuilder(tim::vx::PoolType pool_type) : pool_type_(pool_type) {}
+
+ protected:
+  bool IsOpSupported(const onnxruntime::GraphViewer& graph_viewer, const Node* node) const override {
+    auto shape = vsi::npu::util::GetTensorShape(*node->InputDefs()[0]);
+    if (shape.NumDimensions() == 5) {
+      LOGS_DEFAULT(ERROR) << "3DPool is not supported yet.";
+      return false;
+    }
+
+    NodeAttrHelper helper(*node);
+    if (helper.HasAttr("dilations")) {
+      LOGS_DEFAULT(ERROR) << "NonMaxPool with Dilation parameter is not supported.";
+      return false;
+    }
+    return true;
+  }
+  bool CreatePoolingOp(vsi::npu::GraphEP* graph_ep,
+                       std::vector<std::shared_ptr<tim::vx::Tensor>>& inputs,
+                       std::vector<std::shared_ptr<tim::vx::Tensor>>& outputs,
+                       const std::array<uint32_t, 2>& kernel_size,
+                       const std::array<uint32_t, 2>& strides,
+                       const std::array<uint32_t, 4>& pads,
+                       bool is_global,
+                       const tim::vx::RoundType ceil_mode) {
+    const bool is_1d_pool = inputs[0]->GetShape().size() == 3;
+    std::shared_ptr<tim::vx::Operation> op;
+
+    // Create the appropriate pooling operation
+    if (is_global) {
+      if (is_1d_pool) {
+        op = graph_ep->GetGraph()->CreateOperation<tim::vx::ops::Pool1d>(pool_type_, inputs[0]->GetShape()[0], ceil_mode);
+      } else {
+        std::array<uint32_t, 2> input_size = {inputs[0]->GetShape()[0], inputs[0]->GetShape()[1]};
+        op = graph_ep->GetGraph()->CreateOperation<tim::vx::ops::Pool2d>(pool_type_, input_size, ceil_mode);
+      }
+
+    } else {
+      if (is_1d_pool) {
+        op = graph_ep->GetGraph()->CreateOperation<tim::vx::ops::Pool1d>(pool_type_, std::array<uint32_t, 2>{pads[2], pads[0]}, kernel_size[1], strides[1], ceil_mode);
+      } else {
+        op = graph_ep->GetGraph()->CreateOperation<tim::vx::ops::Pool2d>(pool_type_, pads, kernel_size, strides, ceil_mode);
+      }
+    }
+
+    op->BindInputs(inputs).BindOutputs(outputs);
+    graph_ep->GetOps().push_back(op);
+    return true;
+  }
+  tim::vx::PoolType pool_type_;
+};
+
+class TraditionalPoolOpBuilder : public BasePoolOpBuilder {
+ public:
+  TraditionalPoolOpBuilder() : BasePoolOpBuilder(tim::vx::PoolType::MAX) {}
+
+ protected:
   bool HandleBuildOp(vsi::npu::GraphEP* graph_ep,
                      std::vector<std::shared_ptr<tim::vx::Tensor>>& inputs,
                      std::vector<std::shared_ptr<tim::vx::Tensor>>& outputs,
                      const Node* node) override {
-    LOGS_DEFAULT(VERBOSE) << "Creating GlobalAveragePool Op.";
-    std::array<uint32_t, 2> ksize;
-    ksize[0] = inputs[0]->GetShape()[0];
-    ksize[1] = inputs[0]->GetShape()[1];
-    auto op = graph_ep->GetGraph()->CreateOperation<tim::vx::ops::Pool2d>(
-        tim::vx::PoolType::AVG, ksize);
-    (*op).BindInputs(inputs).BindOutputs(outputs);
-    graph_ep->GetOps().push_back(std::move(op));
-    return true;
+    NodeAttrHelper helper(*node);
+    auto ksize = helper.Get("kernel_shape", std::vector<uint32_t>{1U, 1U});
+    auto strides = helper.Get("strides", std::vector<uint32_t>{1U, 1U});
+    auto pads = helper.Get("pads", std::vector<uint32_t>{0U, 0U, 0U, 0U});
+    tim::vx::RoundType ceil_mode = helper.Get("ceil_mode", 0U) == 0 ? tim::vx::RoundType::FLOOR : tim::vx::RoundType::CEILING;
+    return CreatePoolingOp(graph_ep, inputs, outputs,
+                           {ksize[1], ksize[0]}, {strides[1], strides[0]}, {pads[1], pads[3], pads[0], pads[2]}, false, ceil_mode);
   }
 };
+
+class GlobalPoolOpBuilder : public BasePoolOpBuilder {
+ public:
+  GlobalPoolOpBuilder() : BasePoolOpBuilder(tim::vx::PoolType::MAX) {}
+
+ protected:
+  bool HandleBuildOp(vsi::npu::GraphEP* graph_ep,
+                     std::vector<std::shared_ptr<tim::vx::Tensor>>& inputs,
+                     std::vector<std::shared_ptr<tim::vx::Tensor>>& outputs,
+                     const Node* node) override {
+    NodeAttrHelper helper(*node);
+    tim::vx::RoundType ceil_mode = helper.Get("ceil_mode", 0U) == 0 ? tim::vx::RoundType::FLOOR : tim::vx::RoundType::CEILING;
+    return CreatePoolingOp(graph_ep, inputs, outputs, {}, {}, {}, true, ceil_mode);
+  }
+};
+
+class GlobalAveragePoolOpBuilder : public GlobalPoolOpBuilder {
+ public:
+  GlobalAveragePoolOpBuilder() { pool_type_ = tim::vx::PoolType::AVG; }
+};
+
+class GlobalMaxPoolOpBuilder : public GlobalPoolOpBuilder {
+ public:
+  GlobalMaxPoolOpBuilder() { pool_type_ = tim::vx::PoolType::MAX; }
+};
+
+class AveragePoolOpBuilder : public TraditionalPoolOpBuilder {
+ public:
+  AveragePoolOpBuilder() { pool_type_ = tim::vx::PoolType::AVG; }
+};
+
+class MaxPoolOpBuilder : public TraditionalPoolOpBuilder {
+ public:
+  MaxPoolOpBuilder() { pool_type_ = tim::vx::PoolType::MAX; }
+};
+
 }  // namespace npu
 
 }  // namespace vsi
